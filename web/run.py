b7ce2eaf9a378b8e9024d3474ecaf7b74847c048
fye2357 <59043196+fye2357@users.noreply.github.com>
2020-04-16 18:03:15 +0800

    Update run.py

--- web/run.py
+++ web/run.py
@@ -4,8 +4,8 @@ import os
 
 from app import create_app
 
-config_name = os.getenv('FLASK_CONFIG')
-app = create_app(config_name)
+#config_name = os.getenv('FLASK_CONFIG')
+app = create_app('development')
 
 if __name__ == '__main__':
     app.run()
