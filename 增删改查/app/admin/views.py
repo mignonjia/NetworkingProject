941cc3ae219c055983b08cfdde1fb41de8ea46dc
Mingjia Huo <xkhmj@pku.edu.cn>
2020-06-13 13:33:52 +0800

    merge

--- 增删改查/app/admin/views.py
+++ 增删改查/app/admin/views.py
@@ -2,9 +2,44 @@ from flask import abort, flash, redirect, render_template, url_for
 from flask_login import current_user, login_required
 
 from . import admin
-from forms import DepartmentForm, EmployeeAssignForm, RoleForm
 from .. import db
 from ..models import Department, Employee, Role
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from wtforms.validators import DataRequired
+
+from ..models import Department, Role
+
+
+class DepartmentForm(FlaskForm):
+    """
+    Form for admin to add or edit a department
+    """
+    name = StringField('Name', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class RoleForm(FlaskForm):
+    """
+    Form for admin to add or edit a role
+    """
+    name = StringField('Name', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class EmployeeAssignForm(FlaskForm):
+    """
+    Form for admin to assign departments and roles to employees
+    """
+    department = QuerySelectField(query_factory=lambda: Department.query.all(),
+                                  get_label="name")
+    role = QuerySelectField(query_factory=lambda: Role.query.all(),
+                            get_label="name")
+    submit = SubmitField('Submit')
 
 
 def check_admin():
